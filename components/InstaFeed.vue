0fbd28dd43d837f0d84b540f73724e6711027549
Ashish Aryal <ashish@kortaben.se>
2021-06-23 14:51:00 +0200

    instagram logo and other stuff done

--- components/InstaFeed.vue
+++ components/InstaFeed.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="max-w-hr m-auto images-wrapper grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4   gap-5">
+<div class="max-w-hr m-auto images-wrapper  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5">
   <a v-for="(photo, i) in photos" :key="i" :href="photo.permalink">
     <img  :src="photo.media_url" />
   </a>
