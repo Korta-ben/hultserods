7f308d45434f375ff11a76f0e50c0626b9385b0b
Ashish Aryal <ashish@kortaben.se>
2021-07-07 17:06:16 +0200

    added resopnsive background for homepage header

--- components/HeaderImage.vue
+++ components/HeaderImage.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    <div  class="max-w-full m-auto flex flex-col justify-items-center bg-center bg-no-repeat"
-          style="background-image: url('https://hultapi.kortaben.work/wp-content/uploads/2021/05/Rectangle-7.png')">
-
-
+    <div  class="max-w-full m-auto flex flex-col justify-items-center bg-center bg-no-repeat header-img-bg">
       <div class="self-center leading-8  text-center align-middle py-32">
         <h1 class="text-hultWhite text-6xl hult-heading">Välkommen hit! </h1>
         <h4 class="text-hultWhite ">Här odlar vi snittblommor med omtanke </h4>
